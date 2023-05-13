b3a28d1c856ca516558cc160dab33fff657037bf
raccaricardo <ricardoemanuelracca@gmail.com>
2023-05-13 18:01:21 -0300

    Fist Commit

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+    
+    <h2 class="text-center">Navbar</h2>
+    <main class="container-fluid pb-5 mb-5">
+
+        <slot> </slot>
+        
+    </main>
+<h5 class="text-center mt-5 pt-5">Footer</h5>
+</template>
+
+<script>
+    useHead({
+//          link: [
+//              {
+//                 // rel: "stylesheet",
+//                 // href: "https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css",
+        
+//             },
+            
+//         //     {
+//         //         rel: "stylesheet",
+//         //         href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
+//         //         integrity: "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD",
+//         //         crossorigin: "anonymous",
+//         //     },
+//          ],
+//          
+//          script: [
+//         //     {
+//         //         src:  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js",
+//          
+//             // {
+//             //     src:  "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+//             // },
+//             // {
+//             //     src:  "https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js",
+//             // },
+        
+//             // {
+//             //     src:"localforage.js",
+//             //     body:true,
+//             // },
+//          ],
+    });
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>Index</h1>
+    <div class="d-flex justify-content-center fs-1 mb-3">
+        <div class="spinner-grow text-danger" style="width: 6rem; height: 6rem;" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <div class="mt-5 px-5 d-flex justify-content-center">
+        <div class="mb-3">
+            <label for="exampleInputEmail1 fs-5" class="form-label bg-primary">Test LocalForage</label>
+            <input v-model="inputLocal" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Enter some text and refresh the page to test localstorage</div>
+        </div>
+
+        <button @click="changeLocal()" class="btn btn-primary">Guardar</button>
+    </div>
+    
+    <div class="float-end text-muted fs-3">
+        {{menuStore.someState}}
+
+    </div>
+</template>
+<script setup>
+import { useStore } from '~/store/states';
+
+const menuStore = useStore();
+const inputLocal = ref('');
+const changeLocal = ()=>{
+    menuStore.someState=inputLocal.value;
+}
+</script>
